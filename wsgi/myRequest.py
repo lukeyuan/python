ea52316c74cb4b4489268c94f807c7aa29a20ca4
L600 <l600@ubuntu.ubuntu-domain>
2014-03-08 14:58:51 +0800

    add myRequest

---
+++ wsgi/myRequest.py
@@ -0,0 +1,39 @@
+#-*-coding:utf-8-*-
+
+import sys
+import urllib
+import cgi
+
+import sql
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+def OpenshiftHttpResquest(environ):
+    para = {}
+    try:
+        if "QUERY_STRING" in environ and environ['QUERY_STRING']:
+            string = environ['QUERY_STRING']
+            string = string.split("&") #分解Get
+            for k in string:
+                k = k.split("=")
+                para[k[0]] = k[1]
+        try:
+            bodySize = int(environ['CONTENT_LENGTH'])
+        except ValueError:
+            bodySize = 0
+        body = environ['wsgi.input'].read(bodySize)
+        if body:
+            postData = parse_qs(body)
+            for k,v in d.items():
+                para[k] = v
+
+    except Exception,ex:
+        return Exception,":",ex
+    if para:
+        return str(para)
+    else:
+        return "OK"
+
+def OpenshiftWeChatHttpRequest(environ):
+    return "WeChatOK"
